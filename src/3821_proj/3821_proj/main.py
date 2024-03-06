6ba6a795722ce3e9311337bbf0f3bfa08b995904
seunghwan cha <seunghwancha5857@gmail.com>
2024-03-06 20:18:02 +1100

    Implement A_star

--- src/3821_proj/3821_proj/main.py
+++ src/3821_proj/3821_proj/main.py
@@ -8,12 +8,18 @@ from std_msgs.msg import Header
 from nav_msgs.msg import Odometry                   # Required by /odom
 from nav_msgs.msg import OccupancyGrid              # Required by /map
 from geometry_msgs.msg import PointStamped          # Required by /clicked_point
+from geometry_msgs.msg import PoseWithCovarianceStamped # Required by /initialpose
 
 from visualization_msgs.msg import MarkerArray      # Message to publish debugging message
 from visualization_msgs.msg import Marker
 from std_msgs.msg import ColorRGBA
 
 from geometry_msgs.msg import Point                 # Alternative data structure
+
+import copy                                         # Deep copy
+
+import math
+import heapq                                        # Priority Queue
 
 # from tf2_ros.transform_listener import TransformListener
 # from tf2_ros.buffer import Buffer
@@ -29,6 +35,14 @@ class PointI:
         self.x = x
         self.y = y
         self.z = z
+
+    def __eq__(self, point):
+        return (self.x == point.x) and (self.y == point.y) and (self.z == point.z)
+    
+    def __lt__(self, point):
+        return True
+    
+
 
 class LineDrawer(Node):
     """
@@ -114,6 +128,12 @@ class Map:
 
         return self.Cost_i(x_, y_)
     
+    def SetCost(self, x, y, value):
+        """
+        Set the cost of the cell at [x, y]. Assumes the arguments are valid.
+        """
+        self.map[(int)((self.width * y) + x)] = value
+    
     def ToIndices(self, x, y) -> PointI:
         """
         Convert world coordinate into x,y indices. Assumes arguments are valid.
@@ -131,6 +151,42 @@ class Map:
         a.x = x * self.resolution + self.origin[0]
         a.y = y * self.resolution + self.origin[1]
         return a
+    
+    def CopyBlank(self):
+        """
+        Create a deep copy of this object. Each cell of the grid = 0.
+        """
+        o = copy.deepcopy(self)
+        
+        for i in range(len(o.map)):
+            o.map[i] = 0
+        
+        return o
+
+    def Copy(self):
+        """
+        Create a deep copy of this object.
+        """
+        return copy.deepcopy(self)
+    
+    def Valid(self, point: PointI):
+        """
+        Check if argument is within the size of the map
+        """
+        return (point.x < self.width) and (point.y < self.height)
+    
+
+    def CopySet(self, value):
+        """
+        Create a deep copy of this object and assign each with the the given value
+        """
+
+        o = copy.deepcopy(self)
+        o.map = []
+        for i in range(len(self.map)):
+            o.map.append(value)
+        
+        return o
 
 
 class RosNode(Node):
@@ -145,11 +201,29 @@ class RosNode(Node):
         self.odomSub = self.create_subscription(Odometry, '/odom', self.OnOdomPub, 10)
         self.pointSub = self.create_subscription(PointStamped, '/clicked_point', self.OnPointPub, 10)
 
+        # We will use this instead when deploying to a physical robot
+        self.initPosSub = self.create_subscription(PoseWithCovarianceStamped, '/initialpose', self.OnInitPosePub, 10)
+
         self.lineDrawer = LineDrawer()
 
         self.robotPosition = Point()
         self.receiveData = True
 
+    def OnInitPosePub(self, data:PoseWithCovarianceStamped):
+        
+        #To do: Use this pose instead when we are implementing for the physical robot
+        print('Pose obtained.')
+
+        o = [data.pose.pose.position.x, data.pose.pose.position.y, data.pose.pose.position.z]
+        r = [data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w]
+
+        # self.robotPosition.x = data.pose.pose.position.x
+        # self.robotPosition.y = data.pose.pose.position.y
+        # self.robotPosition.z = data.pose.pose.position.z
+
+        print('Initial Pose published: ')
+        print(f'Position: {o}')
+        print(f'Orient: {r}')
     
     def OnMapPub(self, data:OccupancyGrid):
         print('Map Obtained.')
@@ -193,19 +267,113 @@ class RosNode(Node):
         ############################################
         # Implement a path planning algorithm here #
 
+        a_star = A_star(start, end, self.map)
 
+        path:list[PointI]
+        path = a_star.Run()
 
+        print(f'Size of the path: {len(path)}')
 
-        self.lineDrawer.Append(s.x, s.y)
-        self.lineDrawer.Append(e.x, e.y)
+        for i in range(len(path)):
+            # Convert from index coordinate to world coordinate
+            coords = self.map.ToCoordinates(path[i].x, path[i].y)
+            self.lineDrawer.Append(coords.x, coords.y)
+    
 
-
-
+        #self.lineDrawer.Append(s.x, s.y)
+        #self.lineDrawer.Append(e.x, e.y)
 
         ############################################
 
         self.lineDrawer.End()
         receiveData = True
+
+
+class A_star:
+    
+    def __init__(self, start:PointI, end:PointI, map:Map):
+        self.start = start
+        self.end = end
+        self.map = map
+
+    def Heuristic(self, point:PointI):
+        """
+        Euclidean distance from given point to the destination.
+        Could change to Manhattan distance instead later for experiement.
+        """
+        return math.dist((point.x, point.y), (self.end.x, self.end.y))
+
+    def Run(self):
+        costMap = self.map.CopySet(float('inf'))    # cost map of the same size 
+        print(f'size of costmap: {len(costMap.map)}')
+
+        costMap.SetCost(self.start.x, self.start.y, 0.0)
+
+        predMap = self.map.CopySet(None)            # predecessor map of the same size
+
+        frontier = []                               # Minheap
+        heapq.heappush(frontier, (0.0, self.start)) 
+
+        while len(frontier) > 0:
+            current = heapq.heappop(frontier)
+
+            c_Cost:float
+            c_Vertex:PointI
+
+            c_Cost = current[0]
+            c_Vertex = current[1]
+
+            if c_Vertex == self.end:
+                break
+
+            neighbours = [PointI(c_Vertex.x + 1, c_Vertex.y), PointI(c_Vertex.x - 1, c_Vertex.y), PointI(c_Vertex.x, c_Vertex.y + 1), PointI(c_Vertex.x, c_Vertex.y - 1)]
+
+            for i in range(len(neighbours)):
+                # Don't do anything if the coordinates are invalid.
+                if not self.map.Valid(neighbours[i]) or self.map.Cost_i(neighbours[i].x, neighbours[i].y) >= 1.0:
+                    continue
+
+                cost = c_Cost + self.Heuristic(neighbours[i])
+                # Only consider if the distance is shorter
+                if cost < costMap.Cost_i(neighbours[i].x, neighbours[i].y):
+                    costMap.SetCost(neighbours[i].x, neighbours[i].y, cost) # Update the neighbour's cost
+                    heapq.heappush(frontier, (cost, neighbours[i]))
+
+                    #Update the predecessor array
+                    predMap.SetCost(neighbours[i].x, neighbours[i].y, c_Vertex)
+
+        
+        # Establish a path
+        path = []
+        backTrack = self.end
+
+        while predMap.Cost_i(backTrack.x, backTrack.y) is not None:
+            path.append(backTrack)
+            backTrack = predMap.Cost_i(backTrack.x, backTrack.y)
+        
+        # lastly, append the starting vertex and reverse.
+        path.append(self.start)
+        path.reverse()
+
+        return path
+
+                    
+
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def main(args=None):
