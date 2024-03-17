4c22a95e966e2372a9b9d1dec0bd0db8942bcdc5
seunghwan cha <seunghwancha5857@gmail.com>
2024-03-17 17:08:21 +1100

    Implement more Plotting functions. Examples can be found in path_planner.py, A_start.Run() [line 421] Additionally, created another helper PointI to remove circular dependency issue.

--- src/3821_proj/3821_proj/Helper/Plotter.py
+++ src/3821_proj/3821_proj/Helper/Plotter.py
@@ -1,16 +1,22 @@
 import matplotlib.pyplot as plt
+import matplotlib.colors
+
 import multiprocessing
 from multiprocessing import Process
+
+from .PointI import PointI
+
+import numpy as np
 
 
 
 def _ShowMap(map, title):
 
     plt.title(label=title)
-    plt.imshow(map)
+    plt.imshow(map, cmap='binary')
     plt.show()
 
-def ShowMap(map, title='untitled'):
+def ShowMap(map, title='Map'):
     """
     Display the Map class.
     Parse Map.PlotMap() as argument.
@@ -18,3 +24,56 @@ def ShowMap(map, title='untitled'):
 
     p = Process(target=_ShowMap, args=(map,title))
     p.start()
+
+
+def _ShowCostMap(map, title):
+
+    plt.title(label=title)
+    plt.imshow(map)
+    plt.colorbar()
+    plt.show()
+
+def ShowCostMap(costMap, title='Cost Map'):
+    """
+    Display the cost map.
+    Parse Map.PlotMap() as argument.
+    """
+
+    p = Process(target=_ShowCostMap, args=(costMap,title))
+    p.start()
+
+
+def _ShowPathMap(map, title):
+
+    cmap = matplotlib.colors.ListedColormap(['white', 'black'])
+    cmap.set_bad('green')
+
+    plt.title(label=title)
+    plt.imshow(map, cmap=cmap)
+    plt.show()
+
+
+def ShowPathMap(map, path, title='Generated Path'):
+    """
+    Display the path on the map.
+    Parse Map.PlotMap() for map.
+    path must be a list of PointI
+    """
+    
+    l = np.array(map).astype(float)
+    # Undo the array transformation
+    l = np.fliplr(l)
+    l = np.rot90(l, 3)
+
+    # Mark the path
+    point: PointI
+    for point in path:
+        l[point.y][point.x] = np.nan
+
+    # Do the trnasform again
+    l = np.rot90(l)
+    l = np.fliplr(l)
+
+    p = Process(target=_ShowPathMap, args=(l,title))
+    p.start()
+
