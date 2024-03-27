d5f62e19114d0849f52f4d047998138771f66a35
seunghwan cha <seunghwancha5857@gmail.com>
2024-03-27 16:11:59 +1100

    initial commit

--- src/3821_proj/3821_proj/path_planner.py
+++ src/3821_proj/3821_proj/path_planner.py
@@ -37,6 +37,8 @@ from .Helper.PointI import PointI
 
 import math
 
+import sys
+
 
 # Radius of an arbitrary cylinder that represents the robot, in metres (as rviz uses metres)
 ROBOT_RADIUS = 0.15
@@ -187,7 +189,7 @@ class Map:
         """
         Check if argument is within the size of the map
         """
-        return (point.x < self.width) and (point.y < self.height)
+        return (point.x < self.width) and (point.y < self.height) and (point.x >= 0) and (point.y >= 0)
     
 
     def CopySet(self, value):
@@ -229,7 +231,6 @@ class RosNode(Node):
         # TF
         self.tfBuffer = Buffer()
         self.tfListener = TransformListener(self.tfBuffer, self)
-
 
 
 
@@ -294,6 +295,8 @@ class RosNode(Node):
         start = self.map.ToIndices(s.x, s.y)
         end = self.map.ToIndices(e.x, e.y)
 
+
+
         self.lineDrawer.Begin()
 
         ############################################
@@ -302,11 +305,28 @@ class RosNode(Node):
         #        and It should be named path       #
         ############################################
 
-        a_star = A_star(start, end, self.map)
+        pf = APF(start, end, self.map)
+
+        # Show various maps
+        # Plotter.ShowMap(self.map.PlotMap(), 'Map')
+        # pm = pf.PotentialMap()
+        # Plotter.ShowCostMap(pm.PlotMap(), 'Potential Map')
+        # rm = pf.RepulsiveMap()
+        # Plotter.ShowCostMap(rm.PlotMap(), 'Repulsive Map')
+        # tm = pf.TotalPotentialMap()
+        # Plotter.ShowCostMap(tm.PlotMap(), 'Total Potential')
+        # (x,y) = pf.PotentialGradientMap()
+        # Plotter.ShowGradientArrow(y.PlotMap(), x.PlotMap())
+        # (x1,y1) = pf.RepulsiveGradientMap()
+        # Plotter.ShowGradientArrow(y1.PlotMap(), x1.PlotMap(), "Replusive Gradient")
+        # (x2,y2) = pf.TotalGradientMap()
+        # Plotter.ShowGradientArrow(y2.PlotMap(), x2.PlotMap(), "Total Gradient")
+
 
         path:list[PointI]
-        path = a_star.Run()
+        path = pf.Run()
 
+        Plotter.ShowPathMap(self.map.PlotMap(), path)
         ############################################
 
         print(f'Size of the path: {len(path)}')
@@ -336,7 +356,413 @@ class RosNode(Node):
 
     
 
+class APF:
+    """
+    Implementation of artificial Potential Field
+    """
 
+    def __init__(self, start:PointI, end:PointI, map:Map):
+        self.start = start
+        self.end = end
+        self.map = map
+        self.obstacleDistMap = self.BushFireDistance()
+
+        # Define parameters
+        self.ITERATION_LIMIT = 100000     # Maximum number of iteration allowed
+        self.DISTANCE_TOLERANCE = 0.15   # Threshold at which the robot is considered at the destination
+
+
+        self.ATTRACTIVE_CONST = 0.01 # Constant for attractive potential
+        self.REPULSIVE_CONST = 0.0005 # Constant for repulsive potential
+
+        # Both assumes to be expressed in metre
+        self.ATTRACTIVE_DIST = 0.5 # Range at which the robot is considered close to the goal
+        self.REPULSIVE_DIST = ROBOT_RADIUS * 1.5 # Range at which the robot is considered close to an obstacle.
+        # Make sure the replusive force acts on the robot early so that it has room to rotate away
+
+        # corresponding number of cells
+        self.REPULSIVE_DIST_CELL = self.map.ToIndices(self.map.origin[0] + self.REPULSIVE_DIST, self.map.origin[1]).x
+
+
+
+    def Run(self):
+        """
+        Gradient Descendent Method
+        """
+        p = self.start  # Current point expressed in cell coordinate
+        path = [p]
+        iter = 0
+
+        while iter < self.ITERATION_LIMIT and self.Dist(p, self.end) > self.DISTANCE_TOLERANCE:
+
+            # new point = old point - gradient at old point
+            pGradient = self.TotalGradient(p)
+            pWorld = self.map.ToCoordinates(p.x, p.y)
+            p_new = Point()
+
+            p_new.x = pWorld.x - pGradient.x
+            p_new.y = pWorld.y - pGradient.y
+
+            # Convert the world coordinate back to cell coordinate
+            p_cell = self.map.ToIndices(p_new.x, p_new.y)
+
+            if not self.map.Valid(p_cell):
+                print(f'not valid: {p_cell.x}, {p_cell.y}')
+            p = p_cell
+            path.append(p_cell)
+            iter = iter + 1
+
+
+
+        print(f'Number of Iteration: {iter}')
+        if iter == self.ITERATION_LIMIT:
+            print('ITERATION LIMIT REACHED')
+
+
+        return path
+        
+
+
+
+
+    def PotentialMap(self):
+        pMap = self.map.CopySet(0.0)
+
+        for x in range(pMap.width):
+            for y in range(pMap.height):
+                currentPoint = PointI(x,y)
+                pMap.SetCost(x, y, self.AttractivePotential(currentPoint))
+        
+        return pMap
+    
+
+    def PotentialGradientMap(self):
+        xMap = self.map.CopySet(0.0)
+        yMap = self.map.CopySet(0.0)
+
+        for x in range(self.map.width):
+            for y in range(self.map.height):
+                currentPoint = PointI(x,y)
+                g = self.AttractiveGradient(currentPoint)
+                xMap.SetCost(x, y, g.x)
+                yMap.SetCost(x, y, g.y)
+        
+        return (xMap, yMap)
+    
+
+    def RepulsiveMap(self):
+        pMap = self.map.CopySet(0.0)
+
+        for x in range(pMap.width):
+            for y in range(pMap.height):
+                currentPoint = PointI(x,y )
+                pMap.SetCost(x, y, self.RepulsivePotential(currentPoint))
+
+        return pMap
+
+
+    def RepulsiveGradientMap(self):
+        xMap = self.map.CopySet(0.0)
+        yMap = self.map.CopySet(0.0)
+
+        for x in range(self.map.width):
+            for y in range(self.map.height):
+                currentPoint = PointI(x,y)
+                g = self.RepulsiveGradient(currentPoint)
+                xMap.SetCost(x, y, g.x)
+                yMap.SetCost(x, y, g.y)
+
+        return (xMap, yMap)
+    
+
+    def TotalPotentialMap(self):
+        potential = self.PotentialMap()
+        repulsive = self.RepulsiveMap()
+
+        for x in range(potential.width):
+            for y in range(potential.height):
+                potential.SetCost(x, y, potential.Cost_i(x, y) + repulsive.Cost_i(x, y))
+        
+        return potential
+    
+    def TotalGradientMap(self):
+        (px, py) = self.PotentialGradientMap()
+        (rx, ry) = self.RepulsiveGradientMap()
+
+        for x in range(px.width):
+            for y in range(px.height):
+                px.SetCost(x, y, (px.Cost_i(x, y) + rx.Cost_i(x, y)))
+                py.SetCost(x, y, (py.Cost_i(x, y) + ry.Cost_i(x, y)))
+        return (px, py)
+
+    def BushFireDistance(self):
+        """
+        Compute the distance from each cell to nearest obstacle using bush fire algorithm.
+        """
+
+        # Occupancy grid is interanally 8 bit unsigend int.
+        # Int overflows quite quickly with this data type so make a hard copy with more bits.
+        bushMap = self.map.CopySet(0)
+        visitedMap = self.map.CopySet(False)
+        queue = MinHeap()   # Use heap to fire spread equally. Double buffer approach is probably more efficient.
+
+        for x in range(self.map.width):
+            for y in range(self.map.height):
+                p = PointI(x, y)
+
+                if self.map.Cost_i(x, y) >= 1:
+                    bushMap.SetCost(x, y, 1)    # Occupancy Grid has range of [0, 100]
+                    visitedMap.SetCost(x, y, True)
+
+                    # 8 connectivity
+                    neighbours = [
+                    PointI(p.x + 1, p.y), PointI(p.x - 1, p.y), PointI(p.x, p.y + 1), PointI(p.x, p.y - 1),
+                    PointI(p.x + 1, p.y + 1), PointI(p.x - 1, p.y + 1), PointI(p.x - 1, p.y - 1), PointI(p.x + 1, p.y - 1)
+                            ]
+                    
+                    # Consider all neighbour cells of occupied grid and push it to queue if it's 0.
+                    for neighbour in neighbours:
+                        if self.map.Valid(PointI(neighbour.x, neighbour.y)) and self.map.Cost_i(neighbour.x, neighbour.y) == 0:
+                            queue.Push(0, neighbour.x, neighbour.y)
+
+
+        # Run bush fire.
+        iteration = 1
+        while len(queue) != 0:
+            [_, p] = queue.Pop()
+
+            p = PointI(p[0], p[1])  # Convert tuple to PointI
+
+            # Skip if cell is already visitied
+            if visitedMap.Cost_i(p.x, p.y):
+                continue
+
+            visitedMap.SetCost(p.x, p.y, True)
+
+            minCost = sys.maxsize
+            # 8 connectivity
+            neighbours = [
+            PointI(p.x + 1, p.y), PointI(p.x - 1, p.y), PointI(p.x, p.y + 1), PointI(p.x, p.y - 1),
+            PointI(p.x + 1, p.y + 1), PointI(p.x - 1, p.y + 1), PointI(p.x - 1, p.y - 1), PointI(p.x + 1, p.y - 1)
+                    ]
+            
+            for neighbour in neighbours:
+                if not bushMap.Valid(PointI(neighbour.x, neighbour.y)):
+                    continue
+
+                if  bushMap.Cost_i(neighbour.x, neighbour.y) == 0:
+                    # Push non visited neighbour to queue
+                    queue.Push(iteration, neighbour.x, neighbour.y)
+                else:
+                    bushCost = bushMap.Cost_i(neighbour.x, neighbour.y)
+                    if minCost > bushCost:
+                        minCost = bushCost
+            
+            bushMap.SetCost(p.x, p.y, minCost + 1)
+            iteration = iteration + 1
+
+        return bushMap
+
+            
+    def Dist(self, p1:PointI, p2:PointI):
+        """
+        Distance between two points represented in world coordinate
+        """
+        p1_ = self.map.ToCoordinates(p1.x, p1.y)
+        p2_ = self.map.ToCoordinates(p2.x, p2.y)
+
+        return math.dist([p1_.x, p1_.y], [p2_.x, p2_.y])
+
+
+    def TotalPotential(self, point:PointI):
+        p = self.AttractivePotential(point)
+        r = self.RepulsivePotential(point) 
+        return p + r
+    
+    def TotalGradient(self, point:PointI):
+        p = self.AttractiveGradient(point)
+        r = self.RepulsiveGradient(point)
+
+        po = Point()
+        po.x = p.x + r.x
+        po.y = p.y + r.y
+
+        return po
+
+
+    def RepulsivePotential(self, point:PointI):
+        """
+        Repulsive potential at given cell which is provided as follows:
+
+        P(x) = 1/2*c*(1/d - 1/Q)^2
+        where c is repulsive constant, d is the distance from the cell to obstacle and Q is the effect radius
+        """
+        # Potential is 0 if there are no obstacles within range of effect.
+        if self.obstacleDistMap.Cost_i(point.x, point.y) > self.REPULSIVE_DIST_CELL or self.map.Cost_i(point.x, point.y) >= 1:
+            return 0.0
+        else:
+            # Get the list of obstacles cells that are within the range
+            cells = self.GetObstacleCells(point, self.REPULSIVE_DIST_CELL)
+            
+            val = 0.0
+            for cell in cells:
+                dist = self.Dist(point, cell)
+                val = val + ( (self.REPULSIVE_CONST / 2) * (1/dist - 1/self.REPULSIVE_DIST)**2)
+
+        
+        return val
+    
+    def RepulsiveGradient(self, point:PointI):
+        """
+        Repulsive gradient at a given cell which is as follows:
+        c * (1/Q - 1/d) * 1/d^2 * D
+
+        where D = norm(point - obstacle)
+        """
+        if self.obstacleDistMap.Cost_i(point.x, point.y) > self.REPULSIVE_DIST_CELL or self.map.Cost_i(point.x, point.y) >= 1:
+            # return 0 gradient
+            p = Point()
+            p.x = 0.0
+            p.y = 0.0
+            return p
+        else:
+            cells = self.GetObstacleCells(point, self.REPULSIVE_DIST_CELL) 
+
+            p = Point()
+            p.x = 0.0
+            p.y = 0.0
+
+            # Sum gradient produced by each cell
+            for cell in cells:
+                dist = self.Dist(point, cell)
+
+                const = self.REPULSIVE_CONST * (1/self.REPULSIVE_DIST - 1/dist) * (1/(dist**2))
+
+                p0 = self.map.ToCoordinates(point.x, point.y)
+                p1 = self.map.ToCoordinates(cell.x, cell.y)
+
+                dx = const * (p0.x - p1.x) / dist
+                dy = const * (p0.y - p1.y) / dist
+
+                p.x = p.x + dx
+                p.y = p.y + dy
+
+        return p
+
+            
+
+            
+            
+
+
+
+
+
+
+
+
+
+
+    def GetObstacleCells(self, point:PointI, radius):
+        """
+        Search for all obstacle cells around the point within the radius.
+        Assume the point itself is not an obstacle.
+        """
+        cells = []
+
+        for x in range(point.x - radius, point.x + radius + 1):
+            for y in range(point.y - radius, point.y + radius + 1):
+                if self.map.Valid(PointI(x, y)) and self.map.Cost_i(x, y) >= 1:
+                    cells.append(PointI(x, y))
+        
+        return cells
+
+
+    def AttractivePotential(self, point:PointI):
+        """
+        Attractive potential at given cell
+        """
+
+        d = self.Dist(point, self.end)
+
+        if d <= self.ATTRACTIVE_DIST:
+            # Use quadratic
+            return self.QuadraticPotential(point)
+        else:
+            # attractive_dist * conical - quadratic
+            c = self.ConicalPotential(point)
+            return self.ATTRACTIVE_DIST * c - self.QuadraticPotential(point)
+
+
+
+    def AttractiveGradient(self, point:PointI):
+        """
+        Gradient of attractive potential
+        """
+
+        d = self.Dist(point, self.end)
+
+        if d <= self.ATTRACTIVE_DIST:
+            # quadratic gradient
+            return self.QuadraticGradient(point)
+        else:
+            # attractive_dist * conical gradient
+            p = self.ConicalGradient(point)
+            p.x = p.x * self.ATTRACTIVE_DIST
+            p.y = p.y * self.ATTRACTIVE_DIST
+
+            return p
+
+
+
+    def ConicalPotential(self, point:PointI):
+        """
+        Conical potential of given point.
+        Given a point p, it is defined as follows:
+
+        U(p) = c * d
+
+        where c is attractive potential constant and d is the distance from p to the goal
+        """
+        # Might convert from cell coordinate to world coordinate later.
+        return self.ATTRACTIVE_CONST * self.Dist(point, self.end)
+
+    def ConicalGradient(self, point:PointI) -> Point:
+        """
+        Gradient of conical potential of given point (for euclidean distance).
+        For a point p, it is defined as follows:
+
+        DU(p) = c/d * (p - end)
+        """
+        const = self.ATTRACTIVE_CONST/ self.Dist(point, self.end)
+        pointDiff = PointI(x=point.x - self.end.x, y=point.y - self.end.y)
+
+        p = Point()
+        p.x = pointDiff.x * const
+        p.y = pointDiff.y * const
+        return p
+
+    def QuadraticPotential(self, point:PointI):
+        """
+        Quadratic potential of a given point.
+        Defined as follows:
+
+        U(p) = c/2 d^2
+        """
+        return self.ATTRACTIVE_CONST/2 * self.Dist(point, self.end)**2
+
+
+    def QuadraticGradient(self, point:PointI) -> Point:
+        """
+        Gradient of quadratic potential. Defined as
+
+        DU(p) = c*(p - end)
+        """
+        pointDiff = PointI(x=point.x - self.end.x, y=point.y - self.end.y)
+        p = Point()
+        p.x = pointDiff.x * self.ATTRACTIVE_CONST
+        p.y = pointDiff.y * self.ATTRACTIVE_CONST
+        return p
 
 class A_star:
     """
@@ -384,7 +810,7 @@ class A_star:
             # Second line is the 4 diagonal neighbours.
             neighbours =[
                 PointI(c_Vertex.x + 1, c_Vertex.y), PointI(c_Vertex.x - 1, c_Vertex.y), PointI(c_Vertex.x, c_Vertex.y + 1), PointI(c_Vertex.x, c_Vertex.y - 1),
-                PointI(c_Vertex.x + 1, c_Vertex.y + 1), PointI(c_Vertex.x - 1, c_Vertex.y + 1), PointI(c_Vertex.x + 1, c_Vertex.y - 1), PointI(c_Vertex.x + 1, c_Vertex.y - 1)
+                PointI(c_Vertex.x + 1, c_Vertex.y + 1), PointI(c_Vertex.x - 1, c_Vertex.y + 1), PointI(c_Vertex.x + 1, c_Vertex.y - 1), PointI(c_Vertex.x - 1, c_Vertex.y - 1)
                          ]
 
 
