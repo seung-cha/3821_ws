e0e5fe634da2fac7d8a613037d17579d7ff551e4
kyle <kylee.zhang2@gmail.com>
2024-04-05 00:12:17 +1100

    dijkstra

--- src/3821_proj/3821_proj/path_planner.py
+++ src/3821_proj/3821_proj/path_planner.py
@@ -393,7 +393,7 @@ class A_star:
                 if not self.map.Valid(neighbours[i]) or self.map.Cost_i(neighbours[i].x, neighbours[i].y) >= 1.0:
                     continue
                 
-                cost = c_Cost
+                cost = c_Cost + 1
 
                 # Update the cell if the distance is shorter than the stored one.
                 if cost < costMap.Cost_i(neighbours[i].x, neighbours[i].y):
